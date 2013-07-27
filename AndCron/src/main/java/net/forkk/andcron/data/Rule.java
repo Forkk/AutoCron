7eac1d5a77da4779a2ce7bb6624a6a11a96032d2
Andrew <forkk@forkk.net>
2013-07-27 11:37:13 -0600

    Added the initial interfaces.
    I'm not sure quite how these will turn out, so they're going to change quite a bit over time.

---
+++ AndCron/src/main/java/net/forkk/andcron/data/Rule.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2013 Andrew Okin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.forkk.andcron.data;
+
+/**
+ * An automation rule that specifies when an automation should activate.
+ */
+public interface Rule extends AutomationComponent
+{
+    /**
+     * Initializes the rule. This should set up anything this rule needs. For example, if this is a
+     * geofence rule, this function would set up its geofence.
+     */
+    public abstract void initialize();
+
+    /**
+     * Checks if this rule is active.
+     *
+     * @return If the rule is active, true, else false.
+     */
+    public abstract boolean isActive();
+}
